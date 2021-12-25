3b36ed3753e93626f0340de8002d2181a0e94a4d
Leo <lcheung@swellbottle.com>
2021-12-25 16:34:44 -0500

    set up Theme with ThemeData to customize styles and colors

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,14 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // use themeData to define own colors & styles for all pages
+      // using dark theme and override some values
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        // colorScheme:
+        //     ColorScheme.fromSwatch().copyWith(secondary: Colors.purple),
+      ),
       home: InputPage(),
     );
   }
@@ -26,9 +34,16 @@ class _InputPageState extends State<InputPage> {
       body: Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      // wrap widget with Theme with ThemeData to customize the style of it
+      // floatingActionButton: Theme(
+      //   data: ThemeData(
+      //     colorScheme:
+      //         ColorScheme.fromSwatch().copyWith(secondary: Colors.purple),
+      //   ),
+      //   child: FloatingActionButton(
+      //     child: Icon(Icons.add),
+      //   ),
+      // ),
     );
   }
 }
