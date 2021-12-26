f78460a50e38233095571d5a3993c9f2e8c1c46b
Leo <lcheung@swellbottle.com>
2021-12-26 12:51:37 -0500

    create custom widget for the +/- button

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,6 +19,7 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
   int height = 180;
+  int weight = 60;
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +93,7 @@ class _InputPageState extends State<InputPage> {
                           Text(
                             'cm',
                             style: kLabelTextStyle,
-                          )
+                          ),
                         ],
                       ),
                       // wrap with SliderTheme to override default slider style
@@ -131,6 +132,27 @@ class _InputPageState extends State<InputPage> {
                   Expanded(
                     child: ReusableCard(
                       color: kActiveCardColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Weight (kg)',
+                            style: kLabelTextStyle,
+                          ),
+                          Text(
+                            weight.toString(),
+                            style: kNumberTextStyle,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RoundIconButton(icon: FontAwesomeIcons.minus),
+                              SizedBox(width: 10),
+                              RoundIconButton(icon: FontAwesomeIcons.plus),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                   Expanded(
@@ -160,5 +182,27 @@ class _InputPageState extends State<InputPage> {
         //   ),
         // ),
         );
+  }
+}
+
+// own widget
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({this.icon});
+
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: () {},
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
   }
 }
