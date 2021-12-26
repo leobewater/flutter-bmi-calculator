fa2558bea477caed4da05f38426e43589c53a365
Leo <lcheung@swellbottle.com>
2021-12-26 10:26:05 -0500

    add male and female icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+// constants
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,20 +24,20 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
+                    child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: IconContent(
+                        label: 'MALE',
+                        icon: FontAwesomeIcons.mars,
                       ),
                     ),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
+                    child: ReusableCard(
+                      color: activeCardColor,
+                      cardChild: IconContent(
+                        label: 'FEMALE',
+                        icon: FontAwesomeIcons.venus,
                       ),
                     ),
                   ),
@@ -39,37 +45,31 @@ class _InputPageState extends State<InputPage> {
               ),
             ),
             Expanded(
-              child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
+              child: ReusableCard(
+                color: activeCardColor,
               ),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    child: ReusableCard(
+                      color: activeCardColor,
                     ),
                   ),
                   Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15.0),
-                      decoration: BoxDecoration(
-                        color: Color(0xFF1D1E33),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                    child: ReusableCard(
+                      color: activeCardColor,
                     ),
                   ),
                 ],
               ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity, // full-width
+              height: bottomContainerHeight,
             ),
           ],
         )
@@ -84,5 +84,53 @@ class _InputPageState extends State<InputPage> {
         //   ),
         // ),
         );
+  }
+}
+
+class IconContent extends StatelessWidget {
+  IconContent({@required this.label, @required this.icon});
+
+  final String label;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Icon(
+        icon,
+        size: 80.0,
+      ),
+      SizedBox(
+        height: 15.0,
+      ),
+      Text(
+        label,
+        style: TextStyle(
+          fontSize: 18.0,
+          color: Color(0xFF8D8E98),
+        ),
+      )
+    ]);
+  }
+}
+
+// create own widget - stateless = immutable and variables should be used with "final" so that they can't be changed after decoration.
+class ReusableCard extends StatelessWidget {
+  // constructor and make named variable required
+  ReusableCard({@required this.color, this.cardChild});
+
+  final Color color; // need "final" make it immutable
+  final Widget cardChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
   }
 }
